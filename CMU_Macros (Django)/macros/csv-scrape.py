def633f1d881b5254396629b7722e70a08c1e733
the-srianeesh <srikalva1015@gmail.com>
2026-03-21 16:31:09 -0400

    Add files via upload
    data is cleaned + scraper file attached

--- CMU_Macros (Django)/macros/csv-scrape.py
+++ CMU_Macros (Django)/macros/csv-scrape.py
@@ -1,17 +1,18 @@
 import pandas as pd
 
-file_path = 'abpdata.csv'
+file_path = 'abpdata_cleaned.csv'
 
 df = pd.read_csv(file_path, encoding='latin1')
 
 foodnames = df["Food Names"]
-calories = df["Calories"]
-carbs = df["Carbs"]
-protein = df["Protein"]
-fats = df["Fat"]
+calories = df["Calories (Kcal)"]
+carbs = df["Carbs (g)"]
+protein = df["Protein (g)"]
+fats = df["Fat (g)"]
 
 print(foodnames)
 print(calories)
 print(carbs)
 print(fats)
 print(protein)
+print(df.head)
